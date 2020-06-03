9e239677b11661a8b08dc4573ff05f351f36700f
Adeel <abhaloo@hotmail.com>
2020-06-03 16:11:42 -0600

    Pulling monsters data from API and populating a CardList with ut

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
+import './App.css';
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  //Fetch monsters data from API, convert to JSON, then mount to DOM
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
+  render() {
+      return (
+        <div className="App">
+       
+        <CardList monsters={this.state.monsters}> 
+        
+
+        </CardList>
+          
+        </div>
+      );
+  }
+}
+
+export default App;
