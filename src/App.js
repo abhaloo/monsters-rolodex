a3c291f969636ffd8e0dcd3ff1ae2598a315f5fe
Adeel <abhaloo@hotmail.com>
2020-06-03 20:18:48 -0600

    Created a Search box component This will allow me to re-use the search box elsewhere in the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
 
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
+
 
 
 class App extends Component {
@@ -37,11 +40,11 @@ class App extends Component {
         
         <div className="App">
         
-          <input 
-          type='search' 
-          placeholder='search monster' 
-          onChange={ e => this.setState({ searchField: e.target.value })}/>
           
+          <SearchBox 
+            placeholder = "search monsters"
+            handleChange = {e => this.setState({ searchField: e.target.value })}  
+          />
           <CardList monsters={filteredMonsters}/> 
  
         </div>
