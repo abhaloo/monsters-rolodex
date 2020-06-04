d8a5634d4c44b6353bbf019ca6e3d01099d59b70
Adeel <abhaloo@hotmail.com>
2020-06-03 19:58:46 -0600

    Added search function for monsters You can now search for monsters in a search bar found on top of the page. This was achieved by adding a searchField property to the state, whose input values are then used to filter the list of monsters that is provided to the CardList component in the render() method. So the whole page re-renders and only displays monsters whose names include the input value

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
   //Fetch monsters data from API, convert to JSON, then mount to DOM
+  //uses Promises
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -23,14 +25,25 @@ class App extends Component {
   }
 
   render() {
+      
+    //Pull properties off state and set them to constants
+      const { monsters,searchField } = this.state;  
+      
+      const filteredMonsters = monsters.filter( monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+      
       return (
-        <div className="App">
-       
-        <CardList monsters={this.state.monsters}> 
         
-
-        </CardList>
+        <div className="App">
+        
+          <input 
+          type='search' 
+          placeholder='search monster' 
+          onChange={ e => this.setState({ searchField: e.target.value })}/>
           
+          <CardList monsters={filteredMonsters}/> 
+ 
         </div>
       );
   }
