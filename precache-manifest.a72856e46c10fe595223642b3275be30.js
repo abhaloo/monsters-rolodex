3332780cf9a1b4fc2e12cf666eda1e02c7bc1d4c
Adeel <abhaloo@hotmail.com>
2020-06-07 20:30:38 -0600

    Updates

---
+++ precache-manifest.a72856e46c10fe595223642b3275be30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4248ff4331c748f327fd90bec03f9399",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a3839a44ec54bfac7bbc",
+    "url": "/monsters-rolodex/static/css/main.1c84f510.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3839a44ec54bfac7bbc",
+    "url": "/monsters-rolodex/static/js/main.9d8cf83d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
